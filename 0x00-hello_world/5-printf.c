520e7c93e944279bce6fdf34eea3ee76d3d9a1ad
Iliass Bargache <ibargache6@gmail.com>
2023-07-13 07:17:30 -0700

    mandatory c programming tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+* main - printf .
+* Return: 0 if exited properly, non-zero otherwise
+*/
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+
+}
