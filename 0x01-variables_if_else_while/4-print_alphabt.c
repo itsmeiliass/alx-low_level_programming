9ba80aa690acc6c0d7f4304d12e8a430a48e0ddc
Iliass Bargache <ibargache6@gmail.com>
2023-07-14 06:09:11 -0700

    mnadatory

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-print the alphabet
+ * Return: 0 exit with code 0 if no errors
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	if (letter != 'e' && letter != 'q')
+	{
+		putchar(letter);
+	}
+}
+putchar('\n');
+return (0);
+}
